37e38b8e76642605d9551c8a7220d8a9e25f90af
YOLOO-O <785214433@qq.com>
2026-01-12 17:32:24 +0800

    add Sale.js

---
+++ domain/Sale.js
@@ -0,0 +1,24 @@
+class Sale {
+  constructor(saleId) {
+    this.saleId = saleId;
+    this.items = [];
+    this.totalAmount = 0;
+  }
+
+  addItem(product, quantity = 1) {
+    const item = new SaleItem(product, quantity);
+    this.items.push(item);
+  }
+
+  calculateTotal() {
+    this.totalAmount = this.items.reduce(
+      (sum, item) => sum + item.subTotal,
+      0
+    );
+    return this.totalAmount;
+  }
+
+  completeSale() {
+    console.log("Sale completed. Total:", this.totalAmount);
+  }
+}
