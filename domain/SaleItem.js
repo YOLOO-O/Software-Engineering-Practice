c901733fffc4877beb311bfc1b7908e808e66fee
YOLOO-O <785214433@qq.com>
2026-01-12 17:32:03 +0800

    add SaleItem.js

---
+++ domain/SaleItem.js
@@ -0,0 +1,11 @@
+class SaleItem {
+  constructor(product, quantity) {
+    this.product = product;
+    this.quantity = quantity;
+    this.subTotal = this.calculateSubTotal();
+  }
+
+  calculateSubTotal() {
+    return this.product.getPrice() * this.quantity;
+  }
+}
