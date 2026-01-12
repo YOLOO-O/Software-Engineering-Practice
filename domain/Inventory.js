6bcebc7f8a7c8b743275852cd0e11781f567c059
YOLOO-O <785214433@qq.com>
2026-01-12 17:33:22 +0800

    add Inventory.js

---
+++ domain/Inventory.js
@@ -0,0 +1,26 @@
+class Inventory {
+  constructor() {
+    this.products = new Map();
+  }
+
+  addProduct(product, quantity) {
+    this.products.set(product.productId, {
+      product,
+      quantity
+    });
+  }
+
+  reduceStock(product, quantity) {
+    const record = this.products.get(product.productId);
+    if (record && record.quantity >= quantity) {
+      record.quantity -= quantity;
+    }
+  }
+
+  increaseStock(product, quantity) {
+    const record = this.products.get(product.productId);
+    if (record) {
+      record.quantity += quantity;
+    }
+  }
+}
