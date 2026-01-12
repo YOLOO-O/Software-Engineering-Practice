747e5d8cfca57ec97f755d49b44b85d5de92416b
YOLOO-O <785214433@qq.com>
2026-01-12 17:33:02 +0800

    add Receipt.js

---
+++ domain/Receipt.js
@@ -0,0 +1,22 @@
+class Receipt {
+  constructor(receiptNumber, sale) {
+    this.receiptNumber = receiptNumber;
+    this.sale = sale;
+    this.date = new Date();
+  }
+
+  printReceipt() {
+    console.log("====== RECEIPT ======");
+    console.log("Receipt No:", this.receiptNumber);
+    console.log("Date:", this.date.toLocaleString());
+
+    this.sale.items.forEach(item => {
+      console.log(
+        `${item.product.name} x${item.quantity} = $${item.subTotal}`
+      );
+    });
+
+    console.log("Total: $", this.sale.totalAmount);
+    console.log("=====================");
+  }
+}
