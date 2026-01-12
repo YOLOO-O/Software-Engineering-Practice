4d43e85ebcb0524467bf5be5c3f3216819809285
YOLOO-O <785214433@qq.com>
2026-01-12 17:33:50 +0800

    add Return.js

---
+++ domain/Return.js
@@ -0,0 +1,18 @@
+class Return {
+  constructor(returnId, product, quantity) {
+    this.returnId = returnId;
+    this.product = product;
+    this.quantity = quantity;
+    this.refundAmount = 0;
+  }
+
+  calculateRefund() {
+    this.refundAmount = this.product.getPrice() * this.quantity;
+    return this.refundAmount;
+  }
+
+  processReturn(inventory) {
+    inventory.increaseStock(this.product, this.quantity);
+    console.log("Return processed. Refund:", this.refundAmount);
+  }
+}
